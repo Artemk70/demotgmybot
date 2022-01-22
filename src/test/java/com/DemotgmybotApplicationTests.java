026ee58bc9bfb6fc92c9ffab5898200910a9fecc
Кот Артем <artem70107@gmail.com>
2022-01-22 20:06:29 +0300

    initial commit

---
+++ src/test/java/com/DemotgmybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotgmybotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
